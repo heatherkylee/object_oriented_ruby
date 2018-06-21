affccbfe007602e33803a2f404b1f6197c35266c
Heather Lee <heather.k.lee@gmail.com>
2018-06-20 21:47:11 -0400

    Solution to Question 1

---
+++ pair_programming.rb
@@ -0,0 +1,49 @@
+# Question 1
+# a.
+
+# food_array = []
+
+# 5.times do
+#   food = gets.chomp
+#   food_array << food
+# end
+
+# p food_array
+
+# b.
+
+#Prints output after each food is entered
+# 5.times do
+#   food = gets.chomp
+#   puts "I love " + food
+# end
+
+# puts "I love " + food
+
+#Prints all outputs together
+# food_array = []
+# 5.times do
+#   food = gets.chomp
+#   food_array << food
+# end
+
+# food_array.each do |food|
+#   puts "I love " + food
+# end
+
+# c.
+
+# food_array = []
+# index = 1
+
+# 5.times do
+#   food = gets.chomp
+#   food_array << food
+# end
+
+# food_array.each do |food|
+#   p "#{index}. #{food}"
+#   index += 1
+# end
+
+# Question 2
